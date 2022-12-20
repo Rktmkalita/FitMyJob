cf775bc09cce411f31fabef9d4ef64ad938813f8
Raktim Kalita <rktmkalita2@gmail.com>
2022-12-20 23:01:27 +0530

    complete project upload

---
+++ email-service/src/main/java/com/stackroute/emailservice/model/Signup.java
@@ -0,0 +1,13 @@
+package com.stackroute.emailservice.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Signup {
+    private String email;
+    private String username;
+}
